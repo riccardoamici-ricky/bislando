a66ac6da29fb325fafa3cb6e834655275fc71eff
riccardo amici <amici95@gmail.com>
2020-11-16 17:11:05 +0100

    worst hour of the project

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,2 +1,7 @@
 class Island < ApplicationRecord
+  #belongs_to :user
+  has_many :bookings
+  belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :description, :price, :capacity, presence: true
 end
