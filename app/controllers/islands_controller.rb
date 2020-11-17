6b5f46b19fcf828cc17c2111ca2022c6047fe63e
riccardo amici <amici95@gmail.com>
2020-11-17 16:25:41 +0100

    built footer and let the users create an island

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,5 +1,29 @@
 class IslandsController < ApplicationController
+  def index
+    @islands = Island.all
+  end
+
   def show
     @island = Island.find(params[:id])
+  end
+
+  def new
+    @island = Island.new
+  end
+
+  def create
+    @island = Island.new(island_params)
+
+    if @island.save
+      redirect_to @island, notice: 'island was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def island_params
+    params.require(:island).permit(:name, :description, :price, :type_of_event, :capacity, :photo)
   end
 end
--- app/models/island.rb
+++ app/models/island.rb
@@ -1,5 +1,6 @@
 class Island < ApplicationRecord
   has_many :bookings
+  has_one_attached :photo
   belongs_to :user
   validates :name, presence: true, uniqueness: true
   validates :description, :price, :capacity, presence: true
